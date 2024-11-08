00d1da4cde7d7c708829779929c8ae6a5cae723c
usuariogithub <email@en.github>
2024-11-08 16:16:27 +0000

    feat(examen): add exam mock

--- Biblioteca/DGM/PROGRAMACION/EJERCICIOS/examen_mock.java
+++ Biblioteca/DGM/PROGRAMACION/EJERCICIOS/examen_mock.java
@@ -16,15 +16,57 @@ import java.util.Scanner;
 
 public class examen_mock{
 
+  private static int fiftyBills;
+  private static int twentiesBills;
+  private static int tensBills;
+  private static int fiveBills;
+
+
+  private static void clearConsole(){
+    System.out.print("\033\143");
+  }
+
+
   private static void printInitialMessage(){
     printInConsole("------------------------------------------------------");
     printInConsole("------------------------------------------------------");
     printInConsole("-----------HOLA BIENVENIDO A ESTE CAJERO--------------");
     printInConsole("------------------------------------------------------");
     printInConsole("------------------------------------------------------");
-
     printInConsole("---------INDICA LA CANTIDAD QUE QUIERES RETIRAR--------");
   }
+
+
+  private static void printResult(){
+
+    printInConsole("------------------------------------------------------");
+    printInConsole("------------------------------------------------------");
+    printInConsole("-----------------TUS BILLETES SON---------------------");
+    printInConsole("------------------------------------------------------");
+    printInConsole("------------------------------------------------------");
+    printInConsole("-----------------------DE 50: " + fiftyBills + "----------------------");
+    printInConsole("------------------------------------------------------");
+    printInConsole("-----------------------DE 20: " + twentiesBills + "-----------------------");
+    printInConsole("------------------------------------------------------");
+    printInConsole("-----------------------DE 10: " + tensBills + "-----------------------");
+    printInConsole("------------------------------------------------------");
+    printInConsole("-----------------------DE 5: " + fiveBills + "------------------------");
+
+  }
+
+
+  private static int calculateBills(int quantity){
+        fiftyBills = quantity/50;
+        int fiftiesRest = quantity%50;
+        twentiesBills = fiftiesRest/20;
+        int twentiesRest = fiftiesRest % 20;
+        tensBills = twentiesRest/10;
+        int tesnsRest = twentiesRest % 10;
+        fiveBills = tesnsRest/5;
+        int fiveRest = tesnsRest%5;
+        return fiveRest;
+  }
+
 
   public static void printInConsole(String message){
     System.out.println(message);
@@ -38,21 +80,31 @@ public class examen_mock{
 
     printInitialMessage();
 
+  
+
     int quantity = scanner.nextInt();
     
     boolean isDivisibleByFive = quantity%5==0;
+    int restOfQuantity = quantity%5;
+    clearConsole();
 
     if(isDivisibleByFive){
-       if(quantity>=50){
-        int bills = quantity/50;
-        int rest = quantity&50;
-        printInConsole("SUS billete son: " + bills);
-        printInConsole("SUS resto son: " + rest);
-       }
-
+ 
+    calculateBills(quantity);
+    printResult();
+    scanner.close();
+    return;
     }
 
+    printInConsole("EL NÚMERO " + quantity + " NO ES DIVISIBLE POR 5, PUEDES SACAR " + (quantity - restOfQuantity));
+    int rest = calculateBills(quantity);
 
+    printInConsole("VAS A SACAR " + (quantity - restOfQuantity) + " PORQUE NO HAY BILLETES TAN PEQUEÑOS PARA " + rest + " EUROS");
+
+    printResult();
+    
+
+    scanner.close();
 
   }
 
